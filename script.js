033b9a2b7fbb3196060c4b994be710a66b3fe165
dhruvdharamshi <126825758+dhruvdharamshi@users.noreply.github.com>
2024-01-23 14:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 function updateProgressBar() {
-    const start = new Date('January 1, 2024 00:00:00');
-    const end = new Date('December 31, 2024 23:59:59');
+    const start = new Date(`January 1, 2024 00:00:00`);
+    const end = new Date(`December 31, 2024 23:59:59`);
     const now = new Date();
-    const birthday = new Date('March 17, 2024 00:00:00');
 
     const total = end - start;
     const progress = now - start;
-    const progressPercent = Math.round((progress / total) * 100);
-    const birthdayPercent = Math.round(((birthday - start) / total) * 100);
+    const percentage = Math.round((progress / total) * 100);
 
     const progressBar = document.getElementById('progress-bar');
-    progressBar.style.width = `${progressPercent}%`;
-
-    // Style for the birthday marker
-    progressBar.style.background = `linear-gradient(to right, #4e54c8 ${birthdayPercent}%, #fbc02d ${birthdayPercent}%, #fbc02d ${birthdayPercent + 0.5}%, #4e54c8 ${birthdayPercent + 0.5}%)`;
+    progressBar.style.width = `${percentage}%`;
 
     const percentageText = document.getElementById('percentage');
-    percentageText.textContent = `${progressPercent}% of 2024 has elapsed.`;
+    percentageText.textContent = `${percentage}% of 2024 has elapsed`;
 }
 
+// Update the progress bar every day
 setInterval(updateProgressBar, 86400000);
 updateProgressBar();
